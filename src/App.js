52b40532571bc7515822de9e5a5f9281727186c3
milan <milan3152000@gmail.com>
2025-06-05 20:06:45 +0900

    Fix Stripe checkout: wrap Checkout with Elements

--- src/App.js
+++ src/App.js
@@ -1,39 +1,43 @@
-import {   useEffect} from "react";
- import { useDispatch } from "react-redux";
-import { createAction } from "./utils/reducer/reducer.utils";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Routes, Route } from "react-router-dom";
 
-import { Routes,Route , } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Checkout from "./routes/checkout/checkout.component";
 import Navigation from "./routes/navigation/navigation.component";
- import Authentication from "./routes/authentication/authentication.component";
+import Authentication from "./routes/authentication/authentication.component";
 import Shop from "./routes/shop/shop.component";
+
 import { checkUserSession } from "./store/user/user.action";
 
+import { Elements } from "@stripe/react-stripe-js";
+import { stripePromise } from "./utils/stripe/stripe.utils"; // Đường dẫn đúng file của bạn
+
 const App = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(checkUserSession());
+  }, [dispatch]);
 
-      
-         useEffect(()=>{
-           dispatch(checkUserSession());
-         },[])
-      
-
-
-    return (
-     <Routes>
-      <Route  path='/' element={<Navigation/>}>
-          <Route index element={ <Home/>}/>
-          <Route path='shop/*' element={ <Shop/>}/>
-
-          <Route path='auth' element={ <Authentication/>}/>
-          <Route path='checkout' element={<Checkout/>}/>
-       </Route >
-       </Routes>
-    )
-  
-}
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop/*" element={<Shop />} />
+        <Route path="auth" element={<Authentication />} />
+        {/* Bọc Checkout trong Elements để Stripe hoạt động */}
+        <Route
+          path="checkout"
+          element={
+            <Elements stripe={stripePromise}>
+              <Checkout />
+            </Elements>
+          }
+        />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
-
